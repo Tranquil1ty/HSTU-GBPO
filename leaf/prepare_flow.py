9a6a8474554678c0f54a1b8216c828c6a3026a4a
huqigen03 <huqigen03@kuaishou.com>
2025-05-12 18:06:38 +0800

    auto up

--- leaf/prepare_flow.py
+++ leaf/prepare_flow.py
@@ -10,6 +10,7 @@ class PrepareFlow(LeafFlow, NrApiMixin):
   def _default_flow(self):
     self._pre_pare()
     self._get_abtest_params()
+    self._process_ab_params()
     return self
     
   def _pre_pare(self):
@@ -84,41 +85,6 @@ class PrepareFlow(LeafFlow, NrApiMixin):
         "export_common_attr": "common_filter_photo_set",
         "value_type": "set_int64"
       }]
-    ) \
-    .split_string_list(
-        input_common_attr = "audit_risk_immd_tag_filter_str_gamora",
-        output_common_attr = "audit_risk_immd_tag_list",
-        delimiters=",",
-        parse_to_int=True
-    ) \
-    .split_string_list(
-        input_common_attr = "sexy_content_filter_str_gamora",
-        output_common_attr = "sexy_content_filter_list",
-        delimiters=",",
-        parse_to_int=True
-    ) \
-    .split_string_list(
-        input_common_attr = "impression_audit_second_level_black_tags",
-        output_common_attr = "impression_audit_second_level_black_tag_list",
-        delimiters=",",
-        parse_to_int=True
-    ) \
-    .split_string_list(
-        input_common_attr = "high_hot_audit_second_level_black_tags",
-        output_common_attr = "high_hot_audit_second_level_black_tag_list",
-        delimiters=",",
-        parse_to_int=True
-    ) \
-    .split_string_list(
-        input_common_attr = "topk_audit_second_level_black_tags",
-        output_common_attr = "topk_audit_second_level_black_tag_list",
-        delimiters=",",
-        parse_to_int=True
-    ) \
-    .split_string_list(
-        input_common_attr = "author_type_vv_thresh_type_string",
-        output_common_attr = "author_type_vv_thresh_type_string_list",
-        delimiters=",",
     )
   
   def _get_abtest_params(self):
@@ -168,5 +134,44 @@ class PrepareFlow(LeafFlow, NrApiMixin):
         ],
         deduplicate=True,
         parallel_get=32
+      )
+    )
+  
+  def _process_ab_params(self):
+    return (
+      self.split_string_list(
+          input_common_attr = "audit_risk_immd_tag_filter_str_gamora",
+          output_common_attr = "audit_risk_immd_tag_list",
+          delimiters=",",
+          parse_to_int=True
+      )
+      .split_string_list(
+          input_common_attr = "sexy_content_filter_str_gamora",
+          output_common_attr = "sexy_content_filter_list",
+          delimiters=",",
+          parse_to_int=True
+      )
+      .split_string_list(
+          input_common_attr = "impression_audit_second_level_black_tags",
+          output_common_attr = "impression_audit_second_level_black_tag_list",
+          delimiters=",",
+          parse_to_int=True
+      )
+      .split_string_list(
+          input_common_attr = "high_hot_audit_second_level_black_tags",
+          output_common_attr = "high_hot_audit_second_level_black_tag_list",
+          delimiters=",",
+          parse_to_int=True
+      )
+      .split_string_list(
+          input_common_attr = "topk_audit_second_level_black_tags",
+          output_common_attr = "topk_audit_second_level_black_tag_list",
+          delimiters=",",
+          parse_to_int=True
+      )
+      .split_string_list(
+          input_common_attr = "author_type_vv_thresh_type_string",
+          output_common_attr = "author_type_vv_thresh_type_string_list",
+          delimiters=",",
       )
     )
